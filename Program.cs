e580bb6fdf65abd3454de916003d085e3bff81fc
haroldosales <haroldognu@gmail.com>
2023-04-10 19:44:04 -0300

    settings

--- Program.cs
+++ Program.cs
@@ -10,13 +10,16 @@
 
         Console.WriteLine(name + ", o que voce Deseja");
 
-        Console.WriteLine(@"1 - Adotar um mascote virutal
-                    2 - Ver os mascorte
-                    3 - Sair");
+        Console.WriteLine(@"1 - Adotar um mascote virutal");
+        Console.WriteLine(@"2 - ver os  mascote virutal");
+        Console.WriteLine(@"3 - Sair");
+
+
 
         string escolhar = Console.ReadLine();
         var escolha = new ServicesApi();
        
+        
             switch (escolhar)
             {
                 case "1":
@@ -30,12 +33,13 @@
                     Console.WriteLine("Bulbasaur");
                     Console.WriteLine("Ivysaur");
                     break;
-            
+                    
                 case "3":
                     Console.Clear();
                     break;
 
             }
+        
         
       
         

2c82c141e99f093c5c844374230b8e28b1892b77
haroldosales <haroldognu@gmail.com>
2023-11-09 22:15:56 -0300

    update

--- Program.cs
+++ Program.cs
@@ -1,45 +1,33 @@
-﻿
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        var escolhar = new ServicesApi();
+        string? pokemon = "null";
 
-
-        ServicesApi escolhar  = new ServicesApi();
         Console.WriteLine("Bem vindo ao Tamogtchi ");
 
         Console.WriteLine("Qual é seu nome?");
-        string name = Console.ReadLine();
+        string? name = Console.ReadLine();
 
         Console.WriteLine("--------------Menu-----------------");
 
-        Console.WriteLine(name + ", o que voce Deseja");
-        Console.WriteLine(@"1 - Adotar um mascote virutal");
-
-        Console.WriteLine(@"2 - ver os  mascote virutal");
-        Console.WriteLine(@"3 - Sair");
-
-        string funcao = Console.ReadLine();
-        if (funcao == funcao)
+        while (pokemon != "3")
         {
-            escolhar.OneOne("1");
-            escolhar.OneOne("2");
-            escolhar.OneOne("3");
-            Console.WriteLine("--------------Menu-----------------");
-
-            Console.WriteLine(name + ", o que voce Deseja");
             Console.WriteLine(@"1 - Adotar um mascote virutal");
-
             Console.WriteLine(@"2 - ver os  mascote virutal");
             Console.WriteLine(@"3 - Sair");
+            pokemon = Console.ReadLine();
 
-             funcao = Console.ReadLine();
-
-            switch (funcao)
+            switch (pokemon)
             {
 
                 case "1":
-                    Console.WriteLine(name + ", o que voce Deseja");
+                    Console.WriteLine(name + ", o qual pokemon voce Deseja");
 
-                    funcao = Console.ReadLine();
+                    pokemon = Console.ReadLine();
 
-                    escolhar.dadosPork(funcao);
+                    escolhar.dadosPork(pokemon);
                     break;
                 case "2":
                     escolhar.OneOne("1");
@@ -50,9 +38,13 @@
 
                     break;
                 case "3":
+
                     break;
 
             }
 
-        }
 
+
+        }
+    }
+}
